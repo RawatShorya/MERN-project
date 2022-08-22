654db16f01fd5f342b2b1ddffe010309fb09f93c
Shorya Rawat <rawatshorya@gmail.com>
2022-08-23 03:05:56 +0530

    📝 Error Handling

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -12,16 +12,16 @@ exports.createProduct = catchAsyncErrors(async (req, res, next) => {
 });
 
 //Get All Product
-exports.getAllProducts = async (req, res) => {
+exports.getAllProducts = catchAsyncErrors(async (req, res) => {
   const products = await Product.find();
   res.status(200).json({
     success: true,
     products,
   });
-};
+});
 
 //Get Product Details
-exports.getProductDetails = async (req, res, next) => {
+exports.getProductDetails = catchAsyncErrors(async (req, res, next) => {
   const product = await Product.findById(req.params.id);
   if (!product) {
     return next(new ErrorHander("Product Not Found", 404));
@@ -30,17 +30,14 @@ exports.getProductDetails = async (req, res, next) => {
     success: true,
     product,
   });
-};
+});
 
 //Update Product -- Admin
 
-exports.updateProduct = async (req, res, next) => {
+exports.updateProduct = catchAsyncErrors(async (req, res, next) => {
   let product = await Product.findById(req.params.id);
   if (!product) {
-    return res.status(500).json({
-      success: false,
-      message: "Product not Found",
-    });
+    return next(new ErrorHander("Product Not Found", 404));
   }
   product = await Product.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
@@ -51,17 +48,14 @@ exports.updateProduct = async (req, res, next) => {
     success: true,
     product,
   });
-};
+});
 
 //Delete Product
 
-exports.deleteProduct = async (req, res, next) => {
+exports.deleteProduct = catchAsyncErrors(async (req, res, next) => {
   const product = await Product.findById(req.params.id);
   if (!product) {
-    return res.status(500).json({
-      success: false,
-      message: "Product not found",
-    });
+    return next(new ErrorHander("Product Not Found", 404));
   }
 
   await product.remove();
@@ -69,4 +63,4 @@ exports.deleteProduct = async (req, res, next) => {
     success: true,
     message: "Product Deleted",
   });
-};
+});
